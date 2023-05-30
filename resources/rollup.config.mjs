94d0940810fc9defb5b9ea953c2a66b8b4d94659
Antonio <antoniogviegas@hotmail.com>
2023-05-30 23:55:34 +0200

    Update three.js and rollup

--- resources/rollup.config.mjs
+++ resources/rollup.config.mjs
@@ -1,17 +1,19 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import extensions from './rollup-extensions.mjs';
 import commonjs from "@rollup/plugin-commonjs";
 
 // This creates the bundle used by the examples
 export default {
 	input: "dist/index.js",
-	external: ["three"], // so it's not included
 	output: {
 		file: "./resources/openbim-components.js",
 		format: "esm",
-		inlineDynamicImports: true, // Necessary for jspdf
-		paths: {
-			three: "https://unpkg.com/three@0.135.0/build/three.module.js",
-		},
 	},
-	plugins: [nodeResolve(), commonjs()],
+	plugins: [
+		extensions({
+			extensions: [ '.js' ],
+		}),
+		nodeResolve(),
+		commonjs()
+	],
 };
