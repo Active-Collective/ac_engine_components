93cc8e62b2c61acf16777ec75872f04747123481
Antonio <antoniogviegas@hotmail.com>
2023-06-13 19:19:44 +0200

    Bump version

--- resources/openbim-components.js
+++ resources/openbim-components.js
@@ -5266,12 +5266,21 @@ class SimpleGrid extends Component {
     set visible(visible) {
         this._grid.visible = visible;
     }
+    /** The material of the grid. */
     get material() {
         return this._grid.material;
     }
+    /**
+     * Whether the grid should fade away with distance. Recommended to be true for
+     * perspective cameras and false for orthographic cameras.
+     */
     get fade() {
         return this._fade === 3;
     }
+    /**
+     * Whether the grid should fade away with distance. Recommended to be true for
+     * perspective cameras and false for orthographic cameras.
+     */
     set fade(active) {
         this._fade = active ? 3 : 0;
         this.material.uniforms.uFade.value = this._fade;
