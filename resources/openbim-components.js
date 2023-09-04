183a277b573e5e995f8e3405b85186f72c7426c6
Antonio González Viegas <antoniogviegas@hotmail.com>
2023-09-04 08:47:53 +0200

    Add new tools backend endpoints

--- resources/openbim-components.js
+++ resources/openbim-components.js
@@ -1059,8 +1059,8 @@ class ToolComponent extends Component {
         /** {@link Component.enabled} */
         this.enabled = true;
         this._urls = {
-            base: "https://2fomw59q4h.execute-api.eu-central-1.amazonaws.com/v1/tools/",
-            path: "/contents/index.js?accessToken=",
+            base: "https://dev.api.dev.platform.thatopen.com/v1/tools/",
+            path: "/download?accessToken=",
         };
     }
     /**
