86c4ba20beebec533f0f495805c038ceec1bc197
Juan Hoyos <84806689+HoyosJuan@users.noreply.github.com>
2023-12-21 10:51:39 -0500

    fix(components): 224 - QuerySelector failing on SimpleUICard (#228)
    Removed model ID from SimpleUICard in FragmentCacher

--- resources/openbim-components.js
+++ resources/openbim-components.js
@@ -104613,7 +104613,7 @@ class FragmentCacher extends LocalCacher {
         }
         this.cards = [];
         for (const id of allIDs) {
-            const card = new SimpleUICard(this.components, id);
+            const card = new SimpleUICard(this.components);
             card.title = id;
             this.cards.push(card);
             const deleteCardButton = new Button(this.components, {
@@ -104657,7 +104657,7 @@ class FragmentCacher extends LocalCacher {
         for (const id of ids) {
             if (savedIDs.includes(id))
                 continue;
-            const card = new SimpleUICard(this.components, id);
+            const card = new SimpleUICard(this.components);
             card.title = id;
             this.cards.push(card);
             floatingMenu.addChild(card);
